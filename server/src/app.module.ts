8b43ffbd3adf783902d0a4e09a9d907422a454b8
GiorgiKalatozi <giorgikalatoz@gmail.com>
2024-03-23 21:44:21 +0400

    feat: add users resource

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,9 @@ import * as Joi from 'joi';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './common/database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -14,6 +17,11 @@ import { DatabaseModule } from './common/database/database.module';
       }),
     }),
     DatabaseModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
